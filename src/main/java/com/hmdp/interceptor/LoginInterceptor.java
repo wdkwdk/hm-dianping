cfc60f0f05a90f732e2d3c1422ed1e310317894a
wdk <2031557931@qq.com>
2026-09-16 11:58:04 +0800

    init:初始化项目

---
+++ src/main/java/com/hmdp/interceptor/LoginInterceptor.java
@@ -0,0 +1,35 @@
+package com.hmdp.interceptor;
+
+import com.hmdp.dto.UserDTO;
+import com.hmdp.entity.User;
+import com.hmdp.utils.UserHolder;
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+        if (user == null) {
+            response.setStatus(401);
+            return false;
+        }
+
+        UserHolder.saveUser(new UserDTO(user.getId(), user.getNickName(), user.getIcon()));
+        return true;
+
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+        UserHolder.removeUser();
+    }
+
+}
